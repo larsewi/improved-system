ac0d063e30224321971300ad7d2f45d1bbfd475f
Lars Erik Wik <lars.erik.wik@northern.tech>
2026-02-04 12:02:00 +0100

    Refactored init function as well
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,13 @@ pub fn get_work_dir() -> &'static str {
     WORK_DIR.get().map(|s| s.as_str()).unwrap_or(".improved")
 }
 
-pub fn set_work_dir(path: String) -> Result<(), String> {
+fn set_work_dir(path: String) -> Result<(), String> {
     WORK_DIR.set(path).map_err(|e| e)
+}
+
+pub fn init_impl(path: &str) -> Result<(), String> {
+    env_logger::init();
+    set_work_dir(path.to_string())?;
+    log::debug!("init: work directory: {}", path);
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,26 +10,23 @@ pub mod block {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn init(work_dir: *const c_char) -> i32 {
-    env_logger::init();
-
     if work_dir.is_null() {
         log::error!("init: bad argument: work directory cannot be NULL");
         return -1;
     }
 
-    match unsafe { CStr::from_ptr(work_dir) }.to_str() {
-        Ok(path) => match config::set_work_dir(path.to_string()) {
-            Ok(_) => {
-                log::debug!("init: work directory: {}", path);
-                0
-            }
-            Err(e) => {
-                log::debug!("init: failed to set work directory {}: {}", path, e);
-                -1
-            }
-        },
+    let path = match unsafe { CStr::from_ptr(work_dir) }.to_str() {
+        Ok(path) => path,
         Err(e) => {
             log::error!("init: bad argument: {e}");
+            return -1;
+        }
+    };
+
+    match config::init_impl(path) {
+        Ok(_) => 0,
+        Err(e) => {
+            log::error!("init: {}", e);
             -1
         }
     }
