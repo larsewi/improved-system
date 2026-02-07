c8bd61da66cc7694220a8bd589cc14564633e793
Lars Erik Wik <lars.erik.wik@northern.tech>
2026-02-07 17:45:41 +0100

    Created load and store functions using file locks
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- src/storage.rs
+++ src/storage.rs
@@ -1,10 +1,59 @@
-use std::fs;
-use std::io::Write;
+use std::fs::{self, File};
+use std::io::{Read, Write};
 
+use fs2::FileExt;
 use prost::Message;
 
 use crate::block::Block;
 use crate::config;
+
+/// Stores data to a file in the work directory with an exclusive lock.
+pub fn store(name: &str, data: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
+    let path = config::get_work_dir()?.join(name);
+    log::debug!("Storing data to file '{}'...", path.display());
+
+    let file = File::create(&path)
+        .map_err(|e| format!("Failed to create file '{}': {}", path.display(), e))?;
+    file.lock_exclusive()
+        .map_err(|e| format!("Failed to acquire exclusive lock on '{}': {}", path.display(), e))?;
+
+    (&file)
+        .write_all(data)
+        .map_err(|e| format!("Failed to write to '{}': {}", path.display(), e))?;
+
+    file.unlock()
+        .map_err(|e| format!("Failed to release lock on '{}': {}", path.display(), e))?;
+
+    log::debug!("Stored {} bytes to '{}'", data.len(), path.display());
+    Ok(())
+}
+
+/// Loads data from a file in the work directory with a shared lock.
+pub fn load(name: &str) -> Result<Option<Vec<u8>>, Box<dyn std::error::Error>> {
+    let path = config::get_work_dir()?.join(name);
+    log::debug!("Loading data from file '{}'...", path.display());
+
+    if !path.exists() {
+        log::debug!("File '{}' does not exist", path.display());
+        return Ok(None);
+    }
+
+    let file = File::open(&path)
+        .map_err(|e| format!("Failed to open file '{}': {}", path.display(), e))?;
+    file.lock_shared()
+        .map_err(|e| format!("Failed to acquire shared lock on '{}': {}", path.display(), e))?;
+
+    let mut data = Vec::new();
+    (&file)
+        .read_to_end(&mut data)
+        .map_err(|e| format!("Failed to read from '{}': {}", path.display(), e))?;
+
+    file.unlock()
+        .map_err(|e| format!("Failed to release lock on '{}': {}", path.display(), e))?;
+
+    log::debug!("Loaded {} bytes from '{}'", data.len(), path.display());
+    Ok(Some(data))
+}
 
 pub fn read_block(hash: &str) -> Result<Block, Box<dyn std::error::Error>> {
     let path = config::get_work_dir()?.join(hash);
